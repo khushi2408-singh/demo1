39ab7a073c88161eb771d6631fdda63c94cfad30
khushi <khushisingh24september@gmail.com>
2026-03-25 12:10:54 +0530

    Learning

---
+++ src/LovedOnes.java
@@ -0,0 +1,11 @@
+public class LovedOnes {
+    public static void main(String[] ars){
+        System.out.println("Khushi");
+        System.out.println("Shivani");
+        System.out.println("Hariom");
+        System.out.println("Mummy");
+        System.out.println("papa");
+        System.out.println("Abhay");
+
+    }
+}
